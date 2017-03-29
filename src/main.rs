6c7efb6e943b1d7795a151a9ef47ae7830868a78
Daniel Cook <daniel@danielcook.me>
2017-03-28 23:04:13 -0230

    Switched from GLFW to Glutin Updated CMake to revert changes made for Windows Disabled output of RUSTC_TARGET_TRIPLE which seemed to cause cmake cache parse issues

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,30 @@
 #![crate_name = "scuti"]
 
-extern crate glfw;
+extern crate glutin;
 extern crate gl;
 
-use glfw::{Action, Context, Key};
 use gl::types::*;
 
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-    let (mut window, events) = glfw.create_window(640, 480, "test", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
-
-    window.set_key_polling(true);
-    window.make_current();
-
-    gl::load_with(|s| window.get_proc_address(s) as *const _ );
+    let window = glutin::Window::new().unwrap();
 
 
-    while !window.should_close() {
+    unsafe {
+        window.make_current();
+        gl::load_with(|s| window.get_proc_address(s) as *const _);
+    }
+
+    for event in window.wait_events() {
         unsafe {
             gl::ClearColor(0.3, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
         }
 
         window.swap_buffers();
-    }
-}
-
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
-        _ => {}
     }
 }
